5624f839852ad4260ecde6d56fdb3519b0be6559
sergiormusic <sergioramos@sergios-mbp-2.berklee.net>
2016-10-27 14:45:56 -0400

    START
    Breakout Starting Point

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+
+	int playerLives
+	public void GoalScored(int playerNumber)
+	{
+		// decrese lives when ball hits water
+		if(playerNumber == 1)
+			playerLives--;
+		
+
+		// now check if the player has won
+		if(playerOneScore == 3)
+			GameOver(1);
+		else if (playerTwoScore ==3)
+			GameOver(2);
+
+	} 
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
